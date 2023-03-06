e24275fb9b365e7ef99ee36fd42bb68b9e81b68e
djcesargarcia <djcesargarcia@gmail.com>
2023-03-06 16:40:53 +0000

    Webpage are done in html and css

--- index.php
+++ index.php
@@ -6,10 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap" rel="stylesheet"> 
 </head>
 <body>
-    <?php
-    echo "Hola mundo";
-    ?>
+    <div id="container">
+        <header>
+            <b>Welcome to</b><br><b>Sioux City Gran Canaria</b>
+        </header>
+        <div class="menu">
+            <ul class="menusFather">
+                <li>Home</li>
+                <li>Our company</li>
+                <li>Gallery</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <div class="content">
+            <p class="parrafos">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                In convallis massa non augue egestas aliquam. Integer sed ligula libero. 
+                Nunc sed iaculis ante. Curabitur et bibendum arcu. Suspendisse bibendum lectus nec nisl dictum, 
+                a vestibulum urna dapibus. Mauris dictum varius velit. Praesent ut dui aliquet, finibus enim eu, 
+                semper lectus. Nulla tellus justo, rutrum a lobortis ut, placerat eu tellus. 
+            </p>
+            <p class="parrafos">
+                Donec rutrum, quam posuere convallis interdum, risus eros faucibus eros, sed commodo odio libero eget nunc. 
+                Sed mi tellus, luctus et ligula a, imperdiet varius risus. Sed at sem id felis viverra gravida et quis sem. Phasellus at turpis sapien. Nullam egestas dui quis purus tempus lobortis id at metus. Etiam ut ex posuere, hendrerit mi in, 
+                tempus ipsum. Vivamus viverra nisi quis euismod tincidunt. Aliquam eu enim lacinia, ultrices orci nec, 
+                lacinia justo. Sed ut mattis ipsum. Nunc lectus nisl, varius id lectus ut, laoreet placerat dolor. Curabitur 
+                st lacus, sagittis sed quam eget, porta mattis mi. Nunc vestibulum pretium odio, quis fringilla orci interdum sed. Pellentesque commodo sodales tellus sit amet tempus. 
+            </p>
+        </div>
+         <footer>Copyright@2023 by CesarGarcia</footer>
+    </div>
 </body>
 </html>
